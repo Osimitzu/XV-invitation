03514f9b182821136d602523112c5355aa99baeb
Osimitzu <angel_aocc@hotmail.com>
2023-05-29 05:06:47 -0600

    maps completed

--- js/countDown.js
+++ js/countDown.js
@@ -1,9 +1,9 @@
 simplyCountdown("#count", {
   year: 2023, // required
-  month: 5, // required
-  day: 29, // required
-  hours: 2, // Default is 0 [0-23] integer
-  minutes: 35, // Default is 0 [0-59] integer
+  month: 6, // required
+  day: 30, // required
+  hours: 19, // Default is 0 [0-23] integer
+  minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: {
     //words displayed into the countdown
