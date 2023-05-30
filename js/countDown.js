38936f26c4b496a89945c6b94e803fbbaa21d27b
Osimitzu <angel_aocc@hotmail.com>
2023-05-29 19:11:22 -0600

    information added

--- js/countDown.js
+++ js/countDown.js
@@ -2,7 +2,7 @@ simplyCountdown("#count", {
   year: 2023, // required
   month: 6, // required
   day: 30, // required
-  hours: 19, // Default is 0 [0-23] integer
+  hours: 12, // Default is 0 [0-23] integer
   minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
   words: {
